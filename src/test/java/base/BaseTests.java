6fb4b444be73a45b0b48cdf98277fc2e67e95f46
cristianfchq <cristian.fchq@gmail.com>
2021-07-13 15:35:59 -0400

    adds file of task chapter three

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -21,11 +21,24 @@ public class BaseTests {
 //        driver.manage().window().setSize(new Dimension(375, 812));
 
 //        Search a element by linktext then click in the webelement
-        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
-        inputsLink.click();
+//        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
+//        inputsLink.click();
 
-        List<WebElement> links = driver.findElements(By.tagName("a"));
+//        List<WebElement> links = driver.findElements(By.tagName("a"));
+//        System.out.println(links.size());
+
+        // TASK OF CHAPTER 3
+        WebElement inputsLink2 = driver.findElement(By.linkText("Shifting Content"));
+        inputsLink2.click();
+
+        inputsLink2 = driver.findElement(By.linkText("Example 1: Menu Element"));
+        inputsLink2.click();
+
+        List<WebElement> links = driver.findElements(By.tagName("li"));
         System.out.println(links.size());
+        System.out.println(links.get(1).getLocation());
+        System.out.println(links.get(1).getText());
+        System.out.println(links.get(1).getTagName());
 
         System.out.println(driver.getTitle());
 //        driver.quit();
