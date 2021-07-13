1702ae376d15d4e8af1fd666063b4d3af89a5bb5
cristianfchq <cristian.fchq@gmail.com>
2021-07-13 12:53:48 -0400

    adds file of chapter three

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,8 +1,12 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class BaseTests {
 
@@ -12,10 +16,17 @@ public class BaseTests {
         System.setProperty("webdriver.chrome.driver", "resources\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
-
 //        driver.manage().window().maximize();
 //        driver.manage().window().fullscreen();
-        driver.manage().window().setSize(new Dimension(375, 812));
+//        driver.manage().window().setSize(new Dimension(375, 812));
+
+//        Search a element by linktext then click in the webelement
+        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
+        inputsLink.click();
+
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println(links.size());
+
         System.out.println(driver.getTitle());
 //        driver.quit();
     }
