959beb9a521e84b281e1c240ffc4ce42c263cf2c
cristianfchq <cristian.fchq@gmail.com>
2021-07-14 21:32:02 -0400

    adds file of task of chapter then

--- src/test/java/dropdown/DropdownTests.java
+++ src/test/java/dropdown/DropdownTests.java
@@ -2,7 +2,7 @@ package dropdown;
 
 import base.BaseTests;
 import org.testng.annotations.Test;
-
+import java.util.List;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
@@ -16,6 +16,17 @@ public class DropdownTests extends BaseTests {
         var selectedOptions = dropDownPage.getSelectedOptions();
         assertEquals(selectedOptions.size(), 1, "Incorrect number of selections");
         assertTrue(selectedOptions.contains(option), "Option not selected");
+    }
+
+    @Test
+    public void testAddMultipleAttributeToDropdown() {
+        var dropDownPage = homePage.clickDropDown();
+        dropDownPage.addMultipleAttribute();
+        var optionsToSelect = List.of("Option 1", "Option 2");
+        optionsToSelect.forEach(dropDownPage::selectFromDropDown);
+        var selectedOptions = dropDownPage.getSelectedOptions();
+        assertTrue(selectedOptions.containsAll(optionsToSelect), "All options were not selected");
+        assertEquals(selectedOptions.size(), optionsToSelect.size(), "Number of selected items");
     }
 
 }
